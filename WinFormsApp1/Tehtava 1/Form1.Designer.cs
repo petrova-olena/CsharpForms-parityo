3d24bb39573bee8333c6eb0b888ad2164afef1dc
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2024-10-02 08:59:58 +0300

    02.01.2024
    9:00 start working

---
+++ WinFormsApp1/Tehtava 1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Tehtava_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            OtsikkoLB = new Label();
+            VaihdaBT = new Button();
+            SuspendLayout();
+            // 
+            // OtsikkoLB
+            // 
+            OtsikkoLB.AutoSize = true;
+            OtsikkoLB.Location = new Point(73, 72);
+            OtsikkoLB.Name = "OtsikkoLB";
+            OtsikkoLB.Size = new Size(58, 20);
+            OtsikkoLB.TabIndex = 0;
+            OtsikkoLB.Text = "Otsikko";
+            // 
+            // VaihdaBT
+            // 
+            VaihdaBT.Location = new Point(58, 110);
+            VaihdaBT.Name = "VaihdaBT";
+            VaihdaBT.Size = new Size(94, 65);
+            VaihdaBT.TabIndex = 1;
+            VaihdaBT.Text = "Vaihda teksti";
+            VaihdaBT.UseVisualStyleBackColor = true;
+            VaihdaBT.Click += VaihdaBT_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(474, 336);
+            Controls.Add(VaihdaBT);
+            Controls.Add(OtsikkoLB);
+            Name = "Form1";
+            Text = "Harjoitus 1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label OtsikkoLB;
+        private Button VaihdaBT;
+    }
+}
