21861793f28fb7f0b87c1290430c4108c03869df
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2024-10-03 21:41:03 +0300

    03.10.2024
    Save changes

--- WinFormsApp1/Tehtava 1/obj/Debug/net8.0-windows/Tehtava 1.AssemblyInfo.cs
+++ WinFormsApp1/Tehtava 1/obj/Debug/net8.0-windows/Tehtava 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tehtava 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d45d309f22b593171f9aa70d3ca4b55ef45273")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tehtava 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tehtava 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
